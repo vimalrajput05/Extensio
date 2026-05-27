4b28afb425307b5dddc018ec05ca76643b1791e8
deepapalanikumar379-web <deepapalanikumar379@gmail.com>
2026-05-27 14:17:46 +0530

    herosection added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-white">
       <Navbar />
+            <HeroSection />
+
       
     </div>
   );

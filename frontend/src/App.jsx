e8933a67082b23b51f7207406437fde7614bcfc4
deepapalanikumar379-web <deepapalanikumar379@gmail.com>
2026-05-28 12:06:56 +0530

    frontend finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import HowItWorks from "./components/HowItWorks";
+import FeaturesSection from "./components/FeaturesSection";
+import ProFeatures from "./components/ProFeatures";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-white">
       <Navbar />
             <HeroSection />
-
+<HowItWorks/>
+<FeaturesSection/>
+<ProFeatures/>
+<Footer/>
       
     </div>
   );
